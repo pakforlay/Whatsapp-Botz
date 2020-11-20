a890cc9040abfbfa5060926fb3e388faf77c76f2
pakforlay <60418550+pakforlay@users.noreply.github.com>
2020-11-20 21:39:37 +0700

    Update HandleMsg.js

--- HandleMsg.js
+++ HandleMsg.js
@@ -479,11 +479,11 @@ module.exports = HandleMsg = async (pakforlay, message) => {
                 pakforlay.reply(from, 'Ada yang Error!', id)
             })
             break
-		case 'tiktok':
+	case 'tiktok':
             if (args.length == 0) return pakforlay.reply(from, `Untuk mendownload video tiktok\nketik: ${prefix}tiktok [link_tik]`, id)
-			await pakforlay.reply(from, `_Tunggu, sedang memproses perintah_\n*Jika Bot Tidak mengirim Video. Tunggu Satu Menit, lalu jalani kembali perintah yang dikirimkan.*`, id)
-            const tiktok = await rugaapi.tiktok(args[0])
-            await pakforlay.sendFileFromUrl(from, tiktok, `Sukses mengunduh Video Tiktok tanpa Watermark Menggunakan Bot WhatsApp PakForlay`, id)
+	    await pakforlay.reply(from, `_Tunggu, sedang memproses perintah_\n*Jika Bot Tidak mengirim Video. Tunggu Satu Menit, lalu jalani kembali perintah yang dikirimkan.*`, id)
+            const tiktoks = await rugaapi.tiktok(args[0])
+            await pakforlay.sendFileFromUrl(from, tiktoks, `Sukses mengunduh Video Tiktok tanpa Watermark Menggunakan Bot WhatsApp PakForlay`, id)
             .catch(() => {
                 pakforlay.reply(from, 'Ada yang Error!', id)
             })
